5b0f069853cd813fd04d79a84c7d9a7b9e68fffc
cri2002 <cristianobonnes2021@gmail.com>
2023-03-14 12:49:12 +0100

    inserite immagini in categoria

--- 3a_beta/resources/views/categoria.blade.php
+++ 3a_beta/resources/views/categoria.blade.php
@@ -9,7 +9,7 @@
     <div class="flex justify-center flex-wrap">
         <div class="inline-block mx-2">
             <div class="w-32 h-48 max-w-xs overflow-hidden bg-[#252525]">
-                <div class="w-full h-32 bg-white">
+                <div class="w-full h-32 bg-[url({{asset('/Asset/img/c1.svg')}})]">
                 </div>
                 <div class="w-full h-16 font-['Nunito'] text-white m-1">
                     <h1 class="text-[16px] font-bold truncate h-8">Nike Air Max 270 React</h1>
@@ -24,39 +24,7 @@
         </div>
         <div class="inline-block mx-2">
             <div class="w-32 h-48 max-w-xs overflow-hidden bg-[#252525]">
-                <div class="w-full h-32 bg-white">
-                </div>
-                <div class="w-full h-16 font-['Nunito'] text-white m-1">
-                    <h1 class="text-[16px] font-bold truncate h-8">Nike Air Max 270 React</h1>
-                    <div class="h-6 justify-around flex flex-wrap">
-                        <span class="text-[16px] font-bold text-[#CBEB09]  w-4/6 inline">€ 120,00</span>
-                        {{-- <span class="text-xs text-[#CBEB09]">✡</span> --}}
-                        <div class="bg-[url({{asset('/Asset/img/star.svg')}})] w-1/6 bg-contain bg-no-repeat"></div>
-                        <div class=" w-1/6  "></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="flex justify-center flex-wrap">
-        <div class="inline-block mx-2">
-            <div class="w-32 h-48 max-w-xs overflow-hidden bg-[#252525]">
-                <div class="w-full h-32 bg-white">
-                </div>
-                <div class="w-full h-16 font-['Nunito'] text-white m-1">
-                    <h1 class="text-[16px] font-bold truncate h-8">Nike Air Max 270 React</h1>
-                    <div class="h-6 justify-around flex flex-wrap">
-                        <span class="text-[16px] font-bold text-[#CBEB09]  w-4/6 inline">€ 120,00</span>
-                        {{-- <span class="text-xs text-[#CBEB09]">✡</span> --}}
-                        <div class="bg-[url({{asset('/Asset/img/star.svg')}})] w-1/6 bg-contain bg-no-repeat"></div>
-                        <div class=" w-1/6  "></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="inline-block mx-2">
-            <div class="w-32 h-48 max-w-xs overflow-hidden bg-[#252525]">
-                <div class="w-full h-32 bg-white">
+                <div class="w-full h-32 bg-[url({{asset('/Asset/img/c2.svg')}})]">
                 </div>
                 <div class="w-full h-16 font-['Nunito'] text-white m-1">
                     <h1 class="text-[16px] font-bold truncate h-8">Nike Air Max 270 React</h1>
@@ -73,7 +41,7 @@
     <div class="flex justify-center flex-wrap">
         <div class="inline-block mx-2">
             <div class="w-32 h-48 max-w-xs overflow-hidden bg-[#252525]">
-                <div class="w-full h-32 bg-white">
+                <div class="w-full h-32 bg-[url({{asset('/Asset/img/c3.svg')}})]">
                 </div>
                 <div class="w-full h-16 font-['Nunito'] text-white m-1">
                     <h1 class="text-[16px] font-bold truncate h-8">Nike Air Max 270 React</h1>
@@ -88,7 +56,39 @@
         </div>
         <div class="inline-block mx-2">
             <div class="w-32 h-48 max-w-xs overflow-hidden bg-[#252525]">
-                <div class="w-full h-32 bg-white">
+                <div class="w-full h-32 bg-[url({{asset('/Asset/img/c4.svg')}})]">
+                </div>
+                <div class="w-full h-16 font-['Nunito'] text-white m-1">
+                    <h1 class="text-[16px] font-bold truncate h-8">Nike Air Max 270 React</h1>
+                    <div class="h-6 justify-around flex flex-wrap">
+                        <span class="text-[16px] font-bold text-[#CBEB09]  w-4/6 inline">€ 120,00</span>
+                        {{-- <span class="text-xs text-[#CBEB09]">✡</span> --}}
+                        <div class="bg-[url({{asset('/Asset/img/star.svg')}})] w-1/6 bg-contain bg-no-repeat"></div>
+                        <div class=" w-1/6  "></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center flex-wrap">
+        <div class="inline-block mx-2">
+            <div class="w-32 h-48 max-w-xs overflow-hidden bg-[#252525]">
+                <div class="w-full h-32 bg-[url({{asset('/Asset/img/c5.svg')}})]">
+                </div>
+                <div class="w-full h-16 font-['Nunito'] text-white m-1">
+                    <h1 class="text-[16px] font-bold truncate h-8">Nike Air Max 270 React</h1>
+                    <div class="h-6 justify-around flex flex-wrap">
+                        <span class="text-[16px] font-bold text-[#CBEB09]  w-4/6 inline">€ 120,00</span>
+                        {{-- <span class="text-xs text-[#CBEB09]">✡</span> --}}
+                        <div class="bg-[url({{asset('/Asset/img/star.svg')}})] w-1/6 bg-contain bg-no-repeat"></div>
+                        <div class=" w-1/6  "></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="inline-block mx-2">
+            <div class="w-32 h-48 max-w-xs overflow-hidden bg-[#252525]">
+                <div class="w-full h-32 bg-[url({{asset('/Asset/img/c6.svg')}})]">
                 </div>
                 <div class="w-full h-16 font-['Nunito'] text-white m-1">
                     <h1 class="text-[16px] font-bold truncate h-8">Nike Air Max 270 React</h1>
